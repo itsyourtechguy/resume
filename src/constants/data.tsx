ffb3ed1209219ab3ee38e64a150987b463872c44
itsyourtechguy <crronaldo749@gmail.com>
2025-02-02 05:07:18 +0530

    updated experience

--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -5,7 +5,7 @@ import Resume from "../Ankit Sharma CV.pdf"
 export const headerDetails = {
   image: Image,
   name: "Ankit Sharma",
-  title: "Full Stack Developer",
+  title: "Frontend Developer",
   email: "ankit260196sharma@gmail.com",
   githubLink: "https://github.com/itsyourtechguy",
   linkedin: "https://www.linkedin.com/in/ankit-sharma-17ba89148/",
@@ -20,7 +20,7 @@ export const educationData = [
     title: "M.Sc. in Applied Mathematics",
     institution: "Budapest University of Technology and Economics",
     location: "Budapest, Hungary",
-    duration: "09/2022 – Present",
+    duration: "09/2022 – 01/2025",
     details: [
       "Non-parametric Statistics, Multivariate Statistics, Statistical Programming Package",
       "Financial Process, Stochastic Process and Analysis",
@@ -39,55 +39,41 @@ export const educationData = [
 // Work Details
 export const experienceData = [
   {
-    title: "AP Analyst",
+    title: "Frontend Developer (Part Time)",
     company: "Genpact",
     location: "Budapest, Hungary",
     duration: "12/2023 - Present",
     responsibilities: [
-      "Conducted financial data analysis using SAP, ensuring compliance and accuracy.",
-      "Built Power BI dashboards for visualizing financial metrics, aiding decision-making processes.",
-      "Streamlined workflows by automating repetitive tasks using SQL and Power BI, increasing efficiency by 30%.",
-      "Collaborated with cross-functional teams to ensure alignment on data-driven initiatives.",
+      "Optimized UI components and frontend performance for a seamless user experience.",
+      "Developed reusable frontend components to improve workflow efficiency by 30%.",
     ],
   },
   {
-    title: "Full Stack Developer",
+    title: "Frontend Developer",
     company: "E-ship Simplified",
     location: "Indore, India",
     duration: "03/2021 - 09/2022",
     responsibilities: [
       "Led the development of web applications using React, Redux, and Node.js, focusing on scalability and performance.",
       "Created and maintained RESTful APIs to support seamless data communication between frontend and backend.",
-      "Integrated MongoDB for efficient data storage and retrieval operations.",
+      " Improved performance by 30% using lazy loading, memoization, and efficient state management.",
       "Automated testing and deployment processes through CI/CD pipelines using GitLab.",
-      "Partnered with UX teams to deliver responsive and user-friendly designs.",
+      "Collaborated with UX teams to deliver responsive and user-friendly designs.",
     ],
   },
   {
-    title: "Junior Full Stack Developer",
+    title: "Frontend Developer",
     company: "Dreammill",
     location: "Indore, India",
     duration: "03/2017 - 02/2020",
     responsibilities: [
       "Built interactive SPAs using ReactJS and managed application state with modern JavaScript (ES6+).",
-      "Developed backend services with Node.js and Python for handling server-side logic and data operations.",
-      "Designed and implemented secure RESTful APIs, ensuring efficient and reliable data exchange.",
+      "Created responsive layouts and interactive UI components for seamless user interaction.",
+      "Integrated RESTful APIs and optimized data-fetching strategies for performance.",
       "Utilized Git for version control and GitLab for continuous integration to enhance team workflows.",
       "Ensured high performance and scalability by optimizing code and application architecture.",
     ],
   },
-  //   {
-  //     title: "IT Analyst",
-  //     company: "Gateway International",
-  //     location: "Indore, India",
-  //     duration: "06/2016 - 03/2017",
-  //     responsibilities: [
-  //       "Diagnosed and resolved IT system issues, ensuring minimal downtime and optimal performance.",
-  //       "Optimized database integrity and security through proactive maintenance and analysis.",
-  //       "Automated routine administrative tasks using scripting, increasing operational efficiency by 20%.",
-  //       "Supported database migration projects, ensuring seamless transitions and data accuracy.",
-  //     ],
-  //   },
 ];
 
 // Skills
@@ -98,11 +84,13 @@ export const skillsData: SkillCategory[] = [
       { name: "ReactJS", importance: "high" },
       { name: "Redux", importance: "medium" },
       { name: "JavaScript (ES6+)", importance: "high" },
+      { name: "TypeScript", importance: "high"},
       { name: "Material-UI", importance: "medium" },
       { name: "Tailwind CSS", importance: "high" },
       { name: "NodeJS", importance: "high" },
       { name: "Express.js", importance: "medium" },
       { name: "RESTful APIs", importance: "high" },
+      { name: "Python", importance: "high"},
       { name: "Flask", importance: "low" },
       { name: "Django", importance: "low" },
       { name: "Pandas", importance: "low" },
